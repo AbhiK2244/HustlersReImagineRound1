6cccb55044d8179ddec60840dcb179b0878c1091
Abhishek Kamati <kamtiabhi4@gmail.com>
2024-07-03 12:43:56 +0530

    update font

--- src/components/OrderPageComponents/ThirdPage.jsx
+++ src/components/OrderPageComponents/ThirdPage.jsx
@@ -10,7 +10,7 @@ function ThirdPage()
                 <h1>We'll deliver in</h1>
                 <h1>30 minutes or</h1>
                 <h1>less, or it's <span className="free">free!</span></h1>
-                <p>Your dream pizza is just a tap away!</p>
+                <p id="order-page-content-desc">Your dream pizza is just a tap away!</p>
                 <button className="order-now"><span>ORDER NOW</span></button>
             </div>    
                 <div className="delivery-img">
